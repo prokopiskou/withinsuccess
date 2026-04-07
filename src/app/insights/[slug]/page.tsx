7a8d5933dd5267da32cfa497aa5a63a8b1e081a4
Prokopis Koukis <prokopiskoukis@Mac.home>
2026-04-07 14:49:00 +0300

    Force dynamic rendering for slug pages

--- src/app/insights/[slug]/page.tsx
+++ src/app/insights/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { articles } from "../articles";
+export const dynamic = 'force-dynamic';
 
 export async function generateStaticParams() {
   return articles.map((a) => ({ slug: a.slug }));
