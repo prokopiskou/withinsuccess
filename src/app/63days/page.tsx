d58bf6041bf820f43cd0fc5e55475416021c9a8d
Prokopis Koukis <prokopiskoukis@Mac.home>
2026-04-17 13:21:33 +0300

    add Meta Pixel to 63days landing page

--- src/app/63days/page.tsx
+++ src/app/63days/page.tsx
@@ -136,6 +136,27 @@ function PageContent() {
 
   return (
     <main className="min-h-screen bg-white text-black font-sans">
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '3382009922036740');
+            fbq('track', 'PageView');
+          `
+        }}
+      />
+      <noscript>
+        <img height="1" width="1" style={{display:'none'}}
+          src="https://www.facebook.com/tr?id=3382009922036740&ev=PageView&noscript=1"
+        />
+      </noscript>
 
       {/* HERO + CTA 1 */}
       <section className="pt-20 pb-4 px-6 max-w-3xl mx-auto text-center">
