1bda64dd79e2913e7670bc6e9566151743729b56
Prokopis Koukis <prokopiskoukis@Mac.home>
2026-04-22 15:37:51 +0300

    Fix MetaPixel TypeScript errors

--- src/components/MetaPixel.tsx
+++ src/components/MetaPixel.tsx
@@ -3,38 +3,43 @@ import { useEffect } from "react";
 
 const PIXEL_ID = "1653590555890252";
 
-declare global {
-  interface Window {
-    fbq: (...args: unknown[]) => void;
-    _fbq: unknown;
-  }
-}
-
-export function trackEvent(event: string, params?: object) {
-  if (typeof window === "undefined" || !window.fbq) return;
-  window.fbq("track", event, params);
+export function trackEvent(event: string, params?: Record<string, unknown>) {
+  if (typeof window === "undefined") return;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  if (typeof (window as any).fbq === "undefined") return;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (window as any).fbq("track", event, params);
 }
 
 export default function MetaPixel() {
   useEffect(() => {
-    if (window.fbq) return;
-    const n = function (...args: unknown[]) {
-      (n as unknown as { queue: unknown[][] }).queue.push(args);
-    } as unknown as typeof window.fbq & { queue: unknown[][] };
-    n.queue = [];
-    window.fbq = n;
-    window._fbq = n;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    if ((window as any).fbq) return;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const fbq: any = function (...args: unknown[]) {
+      fbq.queue.push(args);
+    };
+    fbq.queue = [];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (window as any).fbq = fbq;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (window as any)._fbq = fbq;
     const script = document.createElement("script");
     script.async = true;
     script.src = "https://connect.facebook.net/en_US/fbevents.js";
     document.head.appendChild(script);
-    window.fbq("init", PIXEL_ID);
-    window.fbq("track", "PageView");
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (window as any).fbq("init", PIXEL_ID);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (window as any).fbq("track", "PageView");
   }, []);
 
   return (
     <noscript>
-      <img height="1" width="1" style={{ display: "none" }}
+      <img
+        height="1"
+        width="1"
+        style={{ display: "none" }}
         src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`}
       />
     </noscript>
