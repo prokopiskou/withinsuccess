7dd706df9749175cd8c8ffb141d52a1ad60b03e7
Prokopis Koukis <prokopiskoukis@Mac.home>
2026-05-04 13:42:17 +0300

    Add Google Analytics 4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -146,6 +146,18 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-QH4S5H2Z4K"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-QH4S5H2Z4K');
+  `}
+        </Script>
         {/* Meta Pixel Code */}
         <Script
           id="meta-pixel"
