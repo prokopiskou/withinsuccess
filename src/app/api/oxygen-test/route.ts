d0fc3636b65fe99c2be85b109efc40480f24d5bd
Prokopis Koukis <prokopiskoukis@Mac.home>
2026-05-05 13:16:17 +0300

    Add oxygen numbering test

---
+++ src/app/api/oxygen-test/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  const res = await fetch('https://api.oxygen.gr/v1/numbering-sequences', {
+    headers: {
+      'Authorization': `Bearer ${process.env.OXYGEN_API_KEY}`,
+      'Content-Type': 'application/json',
+    }
+  })
+  const data = await res.json()
+  return NextResponse.json(data)
+}
