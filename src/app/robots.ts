af25b928466c9be541d354462366326dbed38d90
Prokopis Koukis <prokopiskoukis@Mac.home>
2026-05-11 01:24:56 +0300

    Fix robots.txt format

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,14 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/api/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/30days/thank-you'],
+      },
+    ],
     sitemap: 'https://withinsuccess.gr/sitemap.xml',
+    host: 'https://withinsuccess.gr',
   }
 }
